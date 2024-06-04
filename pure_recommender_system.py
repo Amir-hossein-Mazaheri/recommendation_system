8744b09b4a6eb7c7d2e1eb49102a123e3e5ccc7a
Amirhossein Mazaheri <mazaheri.a@ec.iut.ac.ir>
2024-06-04 18:57:19 +0330

    fixed to the item based

--- pure_recommender_system.py
+++ pure_recommender_system.py
@@ -3,29 +3,45 @@ from pprint import pprint
 
 users_ratings = []
 
+items_file_encoding = 'ISO-8859-1'
+
 # Create a matrix users in the first column and ratings in other columns
 with open('users.csv', 'r') as file:
     for line in file:
         users_ratings.append([{
             'user_id': int(line.strip().split('|')[0]),
-            'ratings_sum': 0,
+            'ratings_avg': float(0),
             'ratings_count': 0,
-            'most_similar_users': [],
-            'recommended_item': None
         }])
 
-# Fill the matrix based on users rating
-# This matrix is not sparse
+with open('items.csv', 'r', encoding=items_file_encoding) as file:
+    for row in users_ratings:
+        for line in file:
+            [item_id, item_title] = line.strip().split('|')[:2]
+
+            item = {
+                'item_id': int(item_id),
+                'item_title': item_title,
+                'most_similar_items': None,
+                'prediction': None,
+                'user_rating': None
+            }
+
+            row.append(item)
+
+        file.seek(0)
+
+# Fill ratings in the correct position
+# This matrix is very sparse
 with open('ratings.csv', 'r') as file:
     for line in file:
         [user_id, item_id, rating] = map(int, line.strip().split('\t')[:3])
 
-        users_ratings[user_id - 1][0]['ratings_sum'] += rating
+        users_ratings[user_id - 1][0]['ratings_avg'] = (users_ratings[user_id - 1][0]['ratings_avg'] *
+                                                        users_ratings[user_id - 1][0]['ratings_count'] + rating) / (
+                                                               users_ratings[user_id - 1][0]['ratings_count'] + 1)
         users_ratings[user_id - 1][0]['ratings_count'] += 1
-        users_ratings[user_id - 1].append({
-            'item_id': item_id,
-            'user_rating': rating
-        })
+        users_ratings[user_id - 1][item_id]['user_rating'] = rating
 
 
 def limit_precision(number):
@@ -39,64 +55,36 @@ def limit_precision(number):
 
 
 # i1 and i2 are user1 and user2 ratings
-def cosine_similarity(i1, i2, avg1, avg2):
+def cosine_similarity(i1_id, i2_id):
     ruab = 0
     rua = 0
     rub = 0
 
-    for a in i1:
-        for b in i2:
-            if a['item_id'] == b['item_id']:
-                a_adjusted = a['user_rating'] - avg1
-                b_adjusted = b['user_rating'] - avg2
+    for user_rating in users_ratings:
+        i1_rating = user_rating[i1_id]['user_rating']
+        i2_rating = user_rating[i2_id]['user_rating']
 
-                ruab += a_adjusted * b_adjusted
+        if i1_rating is None or i2_rating is None:
+            continue
 
-                rua += a_adjusted ** 2
-                rub += b_adjusted ** 2
+        i1_adjusted_rating = i1_rating - user_rating[0]['ratings_avg']
+        i2_adjusted_rating = i2_rating - user_rating[0]['ratings_avg']
 
-    if rua == 0 or rub == 0:
-        return None
+        ruab += i1_adjusted_rating * i2_adjusted_rating
+        rua += i1_adjusted_rating ** 2
+        rub += i2_adjusted_rating
 
     return ruab / (math.sqrt(rua) * math.sqrt(rub))
 
-
-for row in users_ratings:
-    user = row[0]
-    avg = user['ratings_sum'] / user['ratings_count']
-
-    # Five most similar users
-    most_similar_users = []
-
-    for comparing_row in users_ratings:
-        comparing_user = comparing_row[0]
-
-        # The most similar user is the user itself so to avoid this we won't compare it to itself
-        if comparing_user['user_id'] == user['user_id']:
-            continue
-
-        comparing_avg = comparing_user['ratings_sum'] / comparing_user['ratings_count']
-
-        similarity = cosine_similarity(row[1:], comparing_row[1:], avg, comparing_avg)
-
-        if similarity is None:
-            continue
-
-        if len(most_similar_users) < 5:
-            most_similar_users.append((similarity, comparing_user['user_id']))
-
-            if len(most_similar_users) == 5:
-                most_similar_users.sort(key=lambda i: i[0])
-
-            continue
-
-        if len(most_similar_users) == 5:
-            for index, item in enumerate(most_similar_users):
-                if similarity > item[0]:
-                    most_similar_users[index] = (similarity, comparing_user['user_id'])
-                    break
-
-    user['most_similar_users'] = list(map(lambda i: [limit_precision(i[0]), i[1]], most_similar_users))
-
-for row in users_ratings[:10]:
-    pprint(row[:1])
+# for row in users_ratings:
+#     print(row[0])
+#     none_count = 0
+#     count = 0
+#
+#     for item in row[1:]:
+#         if item['user_rating'] is None:
+#             none_count += 1
+#         else:
+#             count += 1
+#
+#     print(count, none_count, count + none_count)
